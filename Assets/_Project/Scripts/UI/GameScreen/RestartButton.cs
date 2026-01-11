5d854d81f0524ad909be5a7a560e63698a4011fe
Artur <arturgamedev007@gmail.com>
2026-01-11 17:11:36 +0500

    change property button

--- Assets/_Project/Scripts/UI/GameScreen/RestartButton.cs
+++ Assets/_Project/Scripts/UI/GameScreen/RestartButton.cs
@@ -5,6 +5,13 @@ namespace Assets._Project.Scripts.UI.GameScreen
 {
     public class RestartButton : MonoBehaviour
     {
-        public Button Button => GetComponent<Button>();
+        //public Button Button => GetComponent<Button>();
+
+        [field: SerializeField] public Button Button { get; private set; }
+
+        private void OnValidate()
+        {
+            Button = GetComponent<Button>();
+        }
     }
 }
