308bdd1aaeb11fd2e024e08761f04a752c860a3f
Artur <arturgamedev007@gmail.com>
2026-02-18 21:26:33 +0500

    refactor: delete scripts

--- Assets/_Project/Scripts/Player/IInputService.cs
+++
@@ -1,9 +0,0 @@
-namespace _Project.Scripts.Player
-{
-    public interface IInputService
-    {
-        public float Horizontal { get; }
-        public float Vertical { get; }
-        public void Update();
-    }
-}
