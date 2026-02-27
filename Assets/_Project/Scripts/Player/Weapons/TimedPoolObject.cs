07fcd74302df4a60c250a1aa1b38c82ada929856
Artur <arturgamedev007@gmail.com>
2026-02-27 15:10:18 +0500

    refactor: change lifetime

--- Assets/_Project/Scripts/Player/Weapons/TimedPoolObject.cs
+++ Assets/_Project/Scripts/Player/Weapons/TimedPoolObject.cs
@@ -5,7 +5,7 @@ namespace _Project.Scripts.Player.Weapons
 {
     public abstract class TimedPoolObject : MonoBehaviour
     {
-        [SerializeField] private float _lifeTime = 0.5f;
+        [SerializeField] private float _lifeTime = 3.0f;
 
         private Coroutine _coroutine;
         
