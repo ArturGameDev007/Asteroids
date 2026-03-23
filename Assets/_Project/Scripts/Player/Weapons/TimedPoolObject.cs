62317237288bcfc5abcf2e88f4e791b0ec553b7c
Artur <arturgamedev007@gmail.com>
2026-03-23 12:23:03 +0500

    fix: with coroutine change to UniTask

--- Assets/_Project/Scripts/Player/Weapons/TimedPoolObject.cs
+++ Assets/_Project/Scripts/Player/Weapons/TimedPoolObject.cs
@@ -1,19 +1,24 @@
-using System.Collections;
+using System;
+using System.Threading;
 using _Project.Scripts.Configs.Player;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace _Project.Scripts.Player.Weapons
 {
     public abstract class TimedPoolObject : MonoBehaviour
     {
-        [SerializeField] private ShootingConfig  _shootingConfig;
+        [SerializeField] private ShootingConfig _shootingConfig;
 
-        private Coroutine _coroutine;
+        private CancellationTokenSource _cancellationTokenSource;
 
         private void OnEnable()
         {
             StopLifeTimer();
-            _coroutine = StartCoroutine(ReturnRoutine());
+            
+            _cancellationTokenSource = new CancellationTokenSource();
+            
+            ReturnRoutineAsync().Forget();
         }
 
         private void OnDisable()
@@ -23,16 +28,26 @@ namespace _Project.Scripts.Player.Weapons
 
         public void StopLifeTimer()
         {
-            if (_coroutine != null)
-                StopCoroutine(_coroutine);
+            if (_cancellationTokenSource != null)
+            {
+                _cancellationTokenSource.Cancel();
+                _cancellationTokenSource.Dispose();
+                _cancellationTokenSource = null;
+            }
         }
 
-        private IEnumerator ReturnRoutine()
+        private async UniTaskVoid ReturnRoutineAsync()
         {
-            var wait = new WaitForSeconds(_shootingConfig.LifeTime);
-            yield return wait;
-            
-            ReturnToPool();
+            try
+            {
+                int millisecondsDelay = 1000;
+                int delay = (int)(_shootingConfig.LifeTime * millisecondsDelay);
+                
+                await UniTask.Delay(delay, cancellationToken: _cancellationTokenSource.Token);
+                
+                ReturnToPool();
+            }
+            catch (OperationCanceledException){}
         }
 
         protected abstract void ReturnToPool();
